746198804876ca18040b3c1b79f8260c28775fbb
autotaker <autotaker@gmail.com>
2020-12-27 11:52:34 +0900

    add access logger

--- src/Housekeeping/CLI.hs
+++ src/Housekeeping/CLI.hs
@@ -4,7 +4,10 @@ module Housekeeping.CLI(main) where
 import           RIO
 
 import           Housekeeping.API
+import           Network.HTTP.Types.Status
+import           Network.Wai
 import           Network.Wai.Handler.Warp
+
 
 main :: IO ()
 main = do
@@ -12,4 +15,18 @@ main = do
     withLogFunc logOptions $ \lf -> do
         simpleApp <- mkSimpleApp lf Nothing
         runSimpleApp $ logInfo "Server started"
-        run 8080 (app simpleApp)
+        let warpLogger req status mFileSize = runSimpleApp $ logInfo $
+                displayShow method <> ":"
+                <> display code <> ":"
+                <> maybe "-" display mFileSize <> ":"
+                <> displayShow path
+                where
+                method = requestMethod req
+                code = statusCode status
+                path = pathInfo req
+
+
+        let settings = defaultSettings
+                     & setLogger warpLogger
+                     & setPort 8080
+        runSettings settings (app simpleApp)
