a89a799d60194df5094dc0de95e64a61e9338d2f
autotaker <autotaker@gmail.com>
2021-05-12 00:59:06 +0900

    add test for GET /secret

--- test/Housekeeping/Service/Hello/ControllerSpec.hs
+++ test/Housekeeping/Service/Hello/ControllerSpec.hs
@@ -19,20 +19,15 @@ import Network.HTTP.Client (defaultManagerSettings, newManager)
 import Network.HTTP.Types
 import qualified Network.Wai.Handler.Warp as Warp
 import RIO (Display (textDisplay), RIO, Text, catch, runRIO, throwIO, (^.))
+import qualified RIO.ByteString.Lazy as BS
 import Servant
+import Servant.Auth.Client
 import Servant.Auth.Server
-import Servant.Client
-  ( BaseUrl (baseUrlPort),
-    ClientError (FailureResponse),
-    ResponseF (responseStatusCode),
-    client,
-    mkClientEnv,
-    parseBaseUrl,
-    runClientM,
-  )
+import Servant.Client (BaseUrl (baseUrlPort), ClientError (FailureResponse), Response, ResponseF (responseStatusCode), client, mkClientEnv, parseBaseUrl, runClientM)
 import Test.Hspec
   ( Spec,
     around,
+    context,
     describe,
     it,
     runIO,
@@ -70,10 +65,9 @@ testApp config = serveWithContext api ctx $ hoistServerWithContext api ctxProxy 
           (Right <$> runRIO (Env mockHelloHandler) action)
             `catch` (pure . Left)
 
-withTestApp :: (Warp.Port -> IO ()) -> IO ()
-withTestApp =
-  Warp.testWithApplication $
-    testApp <$> defaultSessionConfig
+withTestApp :: SessionConfig -> (Warp.Port -> IO ()) -> IO ()
+withTestApp config =
+  Warp.testWithApplication $ pure $ testApp config
 
 helloAPI :: Proxy ("hello" :> Get '[JSON] Hello)
 helloAPI = Proxy
@@ -96,9 +90,20 @@ insertAPIForm = Proxy
 insertAPIJSON :: Proxy ("message" :> ReqBody '[JSON] MessageForm :> Post '[JSON] ())
 insertAPIJSON = Proxy
 
+secretAPI :: Proxy ("secret" :> Auth '[JWT] User :> Get '[JSON] Hello)
+secretAPI = Proxy
+
+secretAPIUnauthed :: Proxy ("secret" :> Get '[JSON] Hello)
+secretAPIUnauthed = Proxy
+
+isFailure :: (Response -> Bool) -> Either ClientError a -> Bool
+isFailure matcher (Left (FailureResponse _ res)) = matcher res
+isFailure _ _ = False
+
 spec :: Spec
 spec = do
-  around withTestApp $ do
+  config <- runIO defaultSessionConfig
+  around (withTestApp config) $ do
     baseUrl <- runIO $ parseBaseUrl "http://localhost"
     manager <- runIO $ newManager defaultManagerSettings
     let clientEnv port = mkClientEnv manager baseUrl {baseUrlPort = port}
@@ -116,16 +121,12 @@ spec = do
     describe "GET /error" $ do
       it "should return client error response" $ \port -> do
         result <- runClientM (client errorAPI) (clientEnv port)
-        result `shouldSatisfy` \case
-          Left (FailureResponse _ res) -> statusIsClientError (responseStatusCode res)
-          _ -> False
+        result `shouldSatisfy` isFailure (statusIsClientError . responseStatusCode)
 
     describe "GET /fatal" $ do
       it "should return server error response" $ \port -> do
         result <- runClientM (client fatalAPI) (clientEnv port)
-        result `shouldSatisfy` \case
-          Left (FailureResponse _ res) -> statusIsServerError (responseStatusCode res)
-          _ -> False
+        result `shouldSatisfy` isFailure (statusIsServerError . responseStatusCode)
 
     describe "GET /message" $ do
       it "should return list of messages" $ \port -> do
@@ -142,3 +143,15 @@ spec = do
         let form = MessageForm "INSERT TEST"
         result <- runClientM (client insertAPIJSON form) (clientEnv port)
         result `shouldBe` Right ()
+    describe "GET /secret" $ do
+      context "if session is authenticated" $
+        it "should return user name of the current session" $ \port -> do
+          let user = User "user1" 1
+          Right jwt <- makeJWT user (config ^. jwtSettings) Nothing
+          let token = Token $ BS.toStrict jwt
+          result <- runClientM (client secretAPI token) (clientEnv port)
+          result `shouldBe` Right (Secret "user1")
+      context "if session is not authenticated" $
+        it "should return user name of the current session" $ \port -> do
+          result <- runClientM (client secretAPIUnauthed) (clientEnv port)
+          result `shouldSatisfy` isFailure ((== 401) . statusCode . responseStatusCode)
